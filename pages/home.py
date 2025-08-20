d8904d5c563f8c2ec6675b01cbcb8b303b9b87bd
Juan Pablo Narvaez Gomez <juann@ebi.ac.uk>
2025-08-20 11:24:40 +0100

    Adding text descriptions.

--- pages/home.py
+++ pages/home.py
@@ -1,7 +1,7 @@
 # pages/home.py
 from __future__ import annotations
 import dash
-from dash import html
+from dash import html, dcc
 
 dash.register_page(__name__, path="/", name="Home")
 
@@ -9,17 +9,27 @@ layout = html.Main(
     [
         html.H1("Welcome 👋"),
         html.P(
-            "This is a minimal starting point for the genomes dashboard. "
-            "Use the navigation above to switch pages. We’ll add data, tables, and charts step by step."
+            "This is a minimal starting point for the genome annotations dashboard. "
+            "This experimental dashboard helps you explore sequenced genomes with completed annotations in our portal by location, climate, and species. "
+            "You can also view gene biotype counts and examine how they vary across taxonomic groups and environments. "
+            "Use the navigation above to switch pages. "
+            "Use the filters above to explore the data and find species of interest. "
+            "The graphs are interactive — feel free to hover and click to dive deeper."
         ),
         html.Hr(),
-        html.H3("What’s next"),
+        html.H3("Content"),
         html.Ul(
             [
-                html.Li("Populate the global filter dropdowns from your parquet files"),
-                html.Li("Add AG Grid to the Data Browser"),
-                html.Li("Add charts to Genome Annotations"),
+                html.Li("Home - This page"),
+                html.Li("Data Browser - Find species with complete annotations from selected taxon groups, geography, and climate."),
+                html.Li("Genome annotations - Explore genome annotations by taxon ranks, geography, and climate."),
             ]
+        ),
+        html.Hr(),
+        html.H3("Give us feedback!"),
+        html.P(
+            "We'd love your feedback - please share it with us here! \n"
+            "https://docs.google.com/forms/d/e/1FAIpQLSePe_BIU198kMf43gFIvzJFYMdJFwohfxv_sSSIl0WXh05GXQ/viewform?usp=header"
         ),
     ],
     className="page-container",
